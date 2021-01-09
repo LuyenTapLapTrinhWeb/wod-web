dfe756f77c0ea715e9346e41437798ef296a2640
sokkimthanh1 <thanhsk1991@gmail.com>
2021-01-09 07:43:31 +0700

    loading video detail

--- src/app/services/video.service.ts
+++ src/app/services/video.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { delay, map } from 'rxjs/operators';
 import { Video } from '../shared/video';
 import { VIDEOS } from '../shared/videos';
 
@@ -8,9 +8,15 @@ import { VIDEOS } from '../shared/videos';
   providedIn: 'root'
 })
 export class VideoService {
-
   constructor() { }
+
+  getVideo(videoId: string): Observable<Video> {
+    return of(VIDEOS.filter((video) => (video.videoId === videoId))[0]).pipe(delay(2000));
+  }
   getVideos(): Observable<Video[]> {
     return of(VIDEOS).pipe(delay(2000));
+  }
+  getVideoIds(): Observable<string[] | any> {
+    return of(VIDEOS.map(video => video.videoId));
   }
 }
--- src/app/video-detail/video-detail.component.ts
+++ src/app/video-detail/video-detail.component.ts
@@ -1,24 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-
+import { ActivatedRoute, ParamMap, Params, Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { VideoService } from '../services/video.service';
+import { Video } from '../shared/video';
+import { switchMap } from 'rxjs/operators';
 @Component({
   selector: 'app-video-detail',
   templateUrl: './video-detail.component.html',
   styleUrls: ['./video-detail.component.scss']
 })
 export class VideoDetailComponent implements OnInit {
-  location: Location;
-  router: Router;
 
-  constructor() { }
+  router: Router;
+  video: Video;
+  videoIds: string[];
+  prev: string;
+  next: string;
+  index: number;
+  constructor(private location: Location, private videoService: VideoService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.videoService.getVideoIds().subscribe(videoIds => this.videoIds = videoIds);
+    this.route.params.pipe(switchMap((params: Params) => this.videoService.getVideo(params['id'])))
+      .subscribe(video => {
+        this.video = video;
+        this.setNextPrev(video.videoId);
+      });
   }
   onBack(): void {
     if (window.history.length > 1) {
-      // this.location.back();
+      this.location.back();
     } else {
-      this.router.navigate(['/home']);
+      this.router.navigate(['/gioithieu']);
     }
+  }
+  setNextPrev(dishId: string): void {
+    const index = this.index = this.videoIds.indexOf(dishId);
+    this.prev = this.videoIds[(this.videoIds.length + index - 1) % this.videoIds.length];
+    this.next = this.videoIds[(this.videoIds.length + index + 1) % this.videoIds.length];
   }
 }

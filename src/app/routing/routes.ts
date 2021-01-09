2b65dfe1441ff356b93c339424da24fb45618aa4
sokkimthanh1 <thanhsk1991@gmail.com>
2021-01-09 13:07:40 +0700

    fix routting link access welcome page

--- src/app/routing/routes.ts
+++ src/app/routing/routes.ts
@@ -4,9 +4,10 @@ import { VideoComponent } from '../video/video.component';
 import { Routes } from '@angular/router';
 import { VideoDetailComponent } from '../video-detail/video-detail.component';
 export const ROUTES: Routes = [
+  { path: '', component: IntroduceComponent },
   { path: 'gioithieu', component: IntroduceComponent },
   { path: 'video', component: VideoComponent },
   { path: 'video/:id', component: VideoDetailComponent },
-  { path: '', pathMatch: 'full', redirectTo: 'gioithieu' }
+  { path: '', pathMatch: 'full', redirectTo: '' }
   // tslint:disable-next-line:eofline
 ];

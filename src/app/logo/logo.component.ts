ff3af295c95b5b3e1c89009eec6ee76d44d222fc
sokkimthanh1 <thanhsk1991@gmail.com>
2021-01-17 10:59:19 +0700

    config logo responsive

--- src/app/logo/logo.component.ts
+++ src/app/logo/logo.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-logo',
   templateUrl: './logo.component.html',
-  styleUrls: ['./logo.component.scss', './logo-mobile.component.scss']
+  styleUrls: ['./logo.component.scss', './logo.component.mobile.scss']
 })
 export class LogoComponent implements OnInit {
 
